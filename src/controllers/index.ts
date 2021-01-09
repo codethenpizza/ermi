c6a7384d0b1c01696e91488e0f15b4f7e474815d
andreyt <fitality1@gmail.com>
2021-01-09 21:57:14 +0300

    HealthCheck

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,8 +1,10 @@
 import {createController} from "@core/Controller";
 import {AdminController} from "@controllers/admin";
 import {FrontController} from "@controllers/front";
+import {HealthCheck} from "@actions/HealthCheck";
 
 export const Controller = createController([
     {method: 'use', path: '/admin', action: AdminController},
     {method: 'use', path: '/front', action: FrontController},
+    {method: 'get', path: '/', action: HealthCheck}
 ]);
