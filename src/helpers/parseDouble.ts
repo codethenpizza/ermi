79d20179880c00b2f19d79469db59fdf28114391
andreyt <fitality1@gmail.com>
2021-01-28 19:20:37 +0300

    parse price fix

--- src/helpers/parseDouble.ts
+++ src/helpers/parseDouble.ts
@@ -2,7 +2,8 @@ export default function parseDouble (val: string): number {
     if (!val) {
         return null;
     }
-    const value = parseFloat(val.replace(',', '.'));
+    const strVal = val.replace(/[  ]/g, '').replace(',', '.');
+    const value = parseFloat(strVal);
     if (!isNaN(value)) {
         return value;
     } else {
