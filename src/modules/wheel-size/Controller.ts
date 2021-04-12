fcfeab770d11a5f4e20fb90088d0815645d08bf9
andreyt <fitality1@gmail.com>
2021-04-13 01:37:55 +0300

    diameters in wheelsize module

--- src/modules/wheel-size/Controller.ts
+++ src/modules/wheel-size/Controller.ts
@@ -5,6 +5,7 @@ import {ModelListAction} from "./actions/ModelListAction";
 import {WheelsSearchAction} from "./actions/WheelsSearchAction";
 import {GenerationListAction} from "./actions/GenerationListAction";
 import {ModificationsListAction} from "./actions/ModificationsListAction";
+import {DiameterListAction} from "./actions/DiameterListAction";
 
 export const WheelSizeController = createController([
     {method: 'get', path: '/makes', action: MakeListAction},
@@ -12,5 +13,6 @@ export const WheelSizeController = createController([
     {method: 'post', path: '/models', action: ModelListAction},
     {method: 'post', path: '/generations', action: GenerationListAction},
     {method: 'post', path: '/trims', action: ModificationsListAction},
+    {method: 'post', path: '/diameters', action: DiameterListAction},
     {method: 'post', path: '/search', action: WheelsSearchAction},
 ]);
