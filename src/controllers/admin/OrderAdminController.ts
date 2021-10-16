42fb0b309b778e402915eaebe54a6efffe423bd5
AlexU <auteshev505@gmail.com>
2021-10-16 20:12:59 +0300

    update order status

--- src/controllers/admin/OrderAdminController.ts
+++ src/controllers/admin/OrderAdminController.ts
@@ -1,6 +1,8 @@
 import {createController} from "@core/Controller";
 import {OrderListAction} from "@actions/admin/Order/OrderListAction";
+import {OrderUpdateAction} from "@actions/admin/Order/OrderUpdateAction";
 
 export const OrderAdminController = createController([
-    {method: 'get', path: '/list', action: OrderListAction}
+    {method: 'get', path: '/list', action: OrderListAction},
+    {method: 'put', path: '/:id', action: OrderUpdateAction}
 ]);
